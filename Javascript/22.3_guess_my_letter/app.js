38a5ccff18f2118835ae207f9ae1a0e1b0fd40a8
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-10-19 11:04:16 +0300

    exercise 22.3 have been modified

--- Javascript/22.3_guess_my_letter/app.js
+++ Javascript/22.3_guess_my_letter/app.js
@@ -1,16 +1,15 @@
 // const form = document.querySelector('#form');
 const random = document.querySelector('#random');
-const guess = document.querySelector('#input-guess');
+// const guess = document.querySelector('#input-guess');
 const result = document.querySelector('#result');
 const fill = document.querySelector('#fill');
 const appear = document.querySelector('#play-again');
 let guessedLetters=[];
-let randomLetters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z'];
+let randomLetters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let status = true;
-let index = Math.floor(Math.random()*28)-1;
+let index = Math.floor(Math.random()*26)-1;
 let letter = randomLetters[index];
 appear.style.display='none';
-
 function guessMyLetter(event){
    let input = event.key.toLowerCase();
    checkLetter(input,letter)
@@ -22,23 +21,23 @@ function addsToTheGuessedArray(i){
       if (!guessedLetters.includes(i)){
          guessedLetters.push(i)
          if(status){
-            fill.textContent += `${i}, `
+            fill.textContent = guessedLetters.join(',')
          }
       }
    }
 }
 
 function checkLetter(char,l){
-   if(randomLetters.includes(char)){
-      if(l===char){
+   if(randomLetters.includes(char)&&status){
+      if(l===char&&status){
          result.style.color='green';
          result.textContent="Yep that's the right letter";
-         guess.style.display='none'
+         // guess.style.display='none'
          random.textContent=char;
          status = false;
          appear.style.display='block';
       }
-      else if (guessedLetters.includes(char)){
+      else if (guessedLetters.includes(char)&&status){
          result.style.color='blue';
          result.textContent='already used this letter';
       }
@@ -47,11 +46,15 @@ function checkLetter(char,l){
          result.textContent="nope, that's wrong letter";
       }
    }
-   else{
+   else if(!randomLetters.includes(char)){
       result.style.color='yellow';
-         result.textContent="Only valid letters!";
+      result.textContent="Only valid letters!";
       
    }
 }
 
+appear.addEventListener('click',()=>{document.location.reload()})
 document.addEventListener('keyup',guessMyLetter)
+// document.addEventListener('keyup',()=>{
+//    console.log(event.key);
+// })
