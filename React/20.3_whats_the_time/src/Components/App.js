7416b79704418675255fcad7b7f21b97dd206cd0
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-11-20 22:15:34 +0200

    exercise 20.3 have been added!!

---
+++ React/20.3_whats_the_time/src/Components/App.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Time from "./Time";
+
+const AppBox = styled.div`
+   background-color: #282c34;
+   min-height: 100vh;
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+   justify-content: center;
+   font-size: calc(10px + 2vmin);
+   color: white;
+`;
+
+const App = () => {
+   return (
+      <AppBox>
+         <Time />
+      </AppBox>
+   );
+};
+export default App;
