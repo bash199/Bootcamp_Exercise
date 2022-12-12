f8d75d05f09370e8f5e6423f06f1fdaec42f0b3d
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-12-12 17:31:09 +0200

    exercise 16.1_blog_schema have been added..

--- NodeJS/express_routes/app.js
+++ NodeJS/express_routes/app.js
@@ -1,9 +1,10 @@
 import express from "express";
-
+// import { main } from "../16.1_Blog_Schema/mongo.js";
 const app = express();
 const port = 3000;
 
-app.get("/numbers", (req, res) => {
+app.get("/numbers/:name", (req,res)=>{
+   // main(req,res)
    res.send("success using get");
 });
 
@@ -22,3 +23,20 @@ app.delete("/numbers", (req, res) => {
 app.listen(port, () => {
    console.log(` app listening on port ${port}`);
 });
+
+// import {MongoClient} from "mongodb";
+
+// const connectionURL = "mongodb://127.0.0.1:27017";
+// const dbName = "testDB";
+
+// const client = new MongoClient(connectionURL);
+// const db = client.db(dbName);
+
+// export async function main(req, res) {
+//    try {
+//       const data = await db.collection("users").findOne({name: `${req.params.name}`});
+//       res.send(data);
+//    } catch (error) {
+//       res.send(error);
+//    }
+// }
